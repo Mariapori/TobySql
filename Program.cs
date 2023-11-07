c385de314558855d7e393bf1292010077f81f8ac
Topias <topias@mariapori.fi>
2023-11-07 23:12:35 +0200

    Connection form

--- Program.cs
+++ Program.cs
@@ -22,16 +22,17 @@ passEntry.PlaceholderText = "Password";
 pohja.Add(passEntry);
 var connectBtn = new Button();
 connectBtn.Label = "Connect";
-connectBtn.Clicked += delegate{
+connectBtn.Clicked += async delegate{
     connection = new MySqlConnection($"Server={hostEntry.Text};Port={portEntry.Text};Uid={userEntry.Text};Pwd={passEntry.Text};AllowUserVariables=True;");
     try{
     connection.Open();
     if(connection.State == System.Data.ConnectionState.Open){
-        MessageDialog messageDialog = new MessageDialog(window,DialogFlags.Modal,MessageType.Info,ButtonsType.None,"Connected!");
+        MessageDialog messageDialog = new MessageDialog(window,DialogFlags.Modal,MessageType.Info,ButtonsType.Ok,"Connected!");
         messageDialog.Show();
-        messageDialog.Close += async delegate{
+        messageDialog.ButtonPressEvent += async (sender,e) => {
+            messageDialog.Destroy();
+            window.Hide();
             await OpenDatabaseForm(connection);
-            window.Close();
         };
     }
     }catch(Exception ex){
@@ -48,5 +49,14 @@ Application.Run();
 
 async Task OpenDatabaseForm(MySqlConnection connection)
 {
-    var window2 = new Window("TobySql");
+    window = new Window("TobySql");
+    window.Destroyed += (sender,e) => {
+        Application.Quit();
+    };
+    var vbox = new VBox();
+    var connLabel = new Label($"Server version: {connection.ServerVersion}");
+    vbox.Add(connLabel);
+    window.Add(vbox);
+    window.SetDefaultSize(300, 300);
+    window.ShowAll();
 }
