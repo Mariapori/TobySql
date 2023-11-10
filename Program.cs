ec8c30db92a0b25d183b090b0ffd10d1c5241540
Topias Mariapori <topias@mariapori.fi>
2023-11-10 23:51:44 +0200

    Asd

--- Program.cs
+++ Program.cs
@@ -92,6 +92,10 @@ class Program
     static void ShowMessageDialog(Window parent, string message, MessageType messageType)
     {
         var messageDialog = new MessageDialog(parent, DialogFlags.Modal, messageType, ButtonsType.Ok, message);
+        messageDialog.ButtonPressEvent += (s, e) =>
+        {
+            messageDialog.Destroy();
+        };
         messageDialog.Show();
     }
 
@@ -102,7 +106,6 @@ class Program
         return checkbox;
     }
 
-    // Method to open the database form asynchronously
     static async Task OpenDatabaseForm(MySqlConnection connection)
     {
         var databaseWindow = new Window("TobySql");
@@ -110,11 +113,39 @@ class Program
 
         var vbox = new VBox();
 
-        // Label for the database section
         var databaseLabel = new Label("Select Databases:");
 
-        // Create checkboxes for databases from information_schema.schemata
         var databaseCheckboxes = new List<CheckButton>();
+        vbox.Add(databaseLabel);
+        vbox.Add(CreateEntry("Enter SQL Query")); // Adding a placeholder entry for SQL Query
+
+        var runQueryBtn = new Button();
+        runQueryBtn.Label = "Run Query";
+        runQueryBtn.Clicked += async delegate
+        {
+            await RefreshCheckboxList(connection, vbox, databaseCheckboxes);
+        };
+
+        vbox.Add(runQueryBtn);
+
+        databaseWindow.Add(vbox);
+        databaseWindow.SetDefaultSize(400, 300);
+        databaseWindow.ShowAll();
+
+        // Initial population of the checkbox list
+        await RefreshCheckboxList(connection, vbox, databaseCheckboxes);
+    }
+
+    static async Task RefreshCheckboxList(MySqlConnection connection, VBox vbox, List<CheckButton> databaseCheckboxes)
+    {
+        // Clear existing checkboxes
+        foreach (var checkbox in databaseCheckboxes)
+        {
+            vbox.Remove(checkbox);
+        }
+        databaseCheckboxes.Clear();
+
+        // Populate the checkbox list with updated information
         using (var command = new MySqlCommand("SELECT schema_name FROM information_schema.schemata", connection))
         using (var reader = await command.ExecuteReaderAsync())
         {
@@ -127,55 +158,8 @@ class Program
             }
         }
 
-        // Text entry for SQL query
-        var queryEntry = CreateEntry("Enter SQL Query");
-
-        // Button to run the SQL query
-        var runQueryBtn = new Button();
-        runQueryBtn.Label = "Run Query";
-        runQueryBtn.Clicked += async delegate
-        {
-            // Get selected databases
-            var selectedDatabases = databaseCheckboxes
-                .Where(checkbox => checkbox.Active)
-                .Select(checkbox => checkbox.Label)
-                .ToList();
-
-            // Get the SQL query from the textbox
-            var sqlQuery = queryEntry.Text;
-
-            // Execute the SQL query on selected databases
-            try
-            {
-                foreach (var dbName in selectedDatabases)
-                {
-                    using (var command = new MySqlCommand(sqlQuery, connection))
-                    {
-                        command.Parameters.AddWithValue("@dbname", dbName);
-                        using (var reader = await command.ExecuteReaderAsync())
-                        {
-                            // Process the query results as needed
-                        }
-                    }
-                }
-
-                // Show a success message
-                ShowMessageDialog(databaseWindow, "Query executed successfully!", MessageType.Info);
-            }
-            catch (Exception ex)
-            {
-                // Show an error message dialog on query execution failure
-                ShowMessageDialog(databaseWindow, ex.Message, MessageType.Error);
-            }
-        };
-
-        // Add database section, query entry, and run query button to the vbox
-        vbox.Add(databaseLabel);
-        vbox.Add(queryEntry);
-        vbox.Add(runQueryBtn);
-
-        databaseWindow.Add(vbox);
-        databaseWindow.SetDefaultSize(400, 300);
-        databaseWindow.ShowAll();
+        // Redraw the window to reflect the changes
+        vbox.ShowAll();
     }
+
 }
